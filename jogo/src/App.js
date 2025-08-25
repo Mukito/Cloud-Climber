88704336e2ca5fde0266958725c7b2a0ce3027cf
Fabiano Santos Ramos <mukito100@gmail.com>
2025-08-25 14:52:19 -0300

    Create App.js

---
+++ jogo/src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+
+import React from 'react';
+import CloudClimber from './components/Game/CloudClimber.js';
+import './index.css'; // Importar Tailwind CSS
+
+function App() {
+  return (
+    <div className="App">
+      <CloudClimber />
+    </div>
+  );
+}
+
+export default App;
